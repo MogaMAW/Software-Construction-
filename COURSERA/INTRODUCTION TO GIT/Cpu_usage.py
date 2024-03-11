ac2ae76c2ff2ccadb675c3f6b3a66b7c3399c3f1
MOGAMAW <92805881+MogaMAW@users.noreply.github.com>
2024-03-11 11:27:16 +0300

    Add files via upload

---
+++ COURSERA/INTRODUCTION TO GIT/Cpu_usage.py
@@ -0,0 +1,16 @@
+import psutil
+
+def check_cpu_usage(percent):
+
+    usage = psutil.cpu_percent()
+
+    return usage < percent
+
+if not check_cpu_usage(75):
+
+    print("ERROR! CPU is overloaded")
+
+else:
+
+
+    print("Everything ok")
