ee4e6e46d71403a3f1bf2fae3e117fbadeab7704
MOGAMAW <92805881+MogaMAW@users.noreply.github.com>
2024-03-11 11:32:34 +0300

    Add files via upload

---
+++ COURSERA/INTRODUCTION TO GIT/Rearrange_comparrison.py
@@ -0,0 +1,13 @@
+def rearrange_name(name):
+ 
+    result = re.search(r"^([\w .]*), ([\w .]*)$", name)
+    result = re.search(r"^([\w .-]*), ([\w .-]*)$", name)
+ 
+    if result == None:
+ 
+         return name
+ 
+    return "{} {}".format(result[2], result[1])
+
+
+
